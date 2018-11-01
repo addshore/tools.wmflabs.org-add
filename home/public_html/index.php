8b8d67f16e7967d1d2f3633c02fd0e2bf4ee5d11
addshore <addshorewiki@gmail.com>
2018-11-01 18:58:15 +0000

    public_html ... -> ..

--- home/public_html/index.php
+++ home/public_html/index.php
@@ -2,7 +2,7 @@
 
 // The code should be checked out in the src directory in the tool home
 require_once __DIR__ .
-	DIRECTORY_SEPARATOR . '...' .
+	DIRECTORY_SEPARATOR . '..' .
 	DIRECTORY_SEPARATOR . 'src' .
 	DIRECTORY_SEPARATOR . 'web' .
 	DIRECTORY_SEPARATOR . 'index.php';
