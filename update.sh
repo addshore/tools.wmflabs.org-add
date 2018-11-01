4a492d14e0aec04baabf22beee4948a49021d05a
addshore <addshorewiki@gmail.com>
2018-11-01 18:55:40 +0000

    Make update script run updated version of itself

--- update.sh
+++ update.sh
@@ -2,12 +2,22 @@
 
 cd ~/src
 
-git checkout master
-git pull
+if [ $1 -ne "pulled" ]; then
 
-#composer --working-dir ~/src/public_html/api update
+    git checkout master
+    git pull
 
-cp -R ~/src/home/public_html/index.php ~/public_html/index.php
-cp -R ~/src/home/.lighttpd.conf ~/.lighttpd.conf
+    # Call the updated version of this script
+    ~/src/update.sh pulled
 
-webservice restart
+else
+
+    # Nothing to composer update yet
+    #composer --working-dir ~/src/public_html/api update
+
+    cp -R ~/src/home/public_html/index.php ~/public_html/index.php
+    cp -R ~/src/home/.lighttpd.conf ~/.lighttpd.conf
+
+    webservice restart
+
+fi
