4753a8e53919bd8e23452d9235532c26f98aa24e
addshore <addshorewiki@gmail.com>
2018-11-02 11:31:11 +0000

    Add src/Wikimedia/Gerrit/CachedGerrit.php

---
+++ src/Wikimedia/Gerrit/CachedGerrit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Addtool\Wikimedia\Gerrit;
+
+use Addtool\SimpleCache\SimpleCache;
+
+class CachedGerrit
+{
+	private $gerritUrl;
+	private $simpleCache;
+
+	public function __construct(
+		string $gerritUrl,
+		SimpleCache $simpleCache
+	)
+	{
+		$this->gerritUrl = $gerritUrl;
+		$this->simpleCache = $simpleCache;
+	}
+
+	/**
+	 * @param string $path Example: /r/changes/1234
+	 */
+	public function getRequest( string $path ) {
+		return $this->simpleCache->do_curl( $this->gerritUrl . $path );
+	}
+
+}
