1330591e96a5f1c953d55e403a0b5ceeb8570a7f
addshore <addshorewiki@gmail.com>
2018-11-02 09:48:22 +0000

    dep injection using pimple

--- web/index.php
+++ web/index.php
@@ -2,9 +2,21 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+$container = new Pimple\Container();
+
+$container['simplecachefactory'] = function ($c) {
+	return new \Addtool\SimpleCache\SimpleCacheFactory();
+};
+
+$container['requesthandler_helloworld'] = function ($c) {
+	return new \Addtool\Slim\UseCases\HelloWorld\RequestHandler();
+};
+$container['wikimedia_gerrit_changeidextractor'] = function ($c) {
+	return new \Addtool\Wikimedia\Gerrit\UrlChangeIdExtractor();
+};
 
 $app = new Slim\App();
 
-$app->get('/helloworld/{name}', [ new \Addtool\Slim\UseCases\HelloWorld\RequestHandler(), 'handle' ] );
+$app->get('/helloworld/{name}', [ $container['requesthandler_helloworld'], 'handle' ] );
 
 $app->run();
