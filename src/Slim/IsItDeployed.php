66260ac873f289b1c292eec211f50e853d4e55c5
addshore <addshorewiki@gmail.com>
2018-11-02 10:22:06 +0000

    isitdeployed, extract changeid from url

--- src/Slim/IsItDeployed.php
+++ src/Slim/IsItDeployed.php
@@ -2,13 +2,26 @@
 
 namespace Addtool\Slim;
 
+use Addtool\Wikimedia\Gerrit\UrlChangeIdExtractor;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class IsItDeployed implements RequestHandler {
 
+	private $changeIdExtractor;
+
+	public function __construct(
+		UrlChangeIdExtractor $changeIdExtractor
+	)
+	{
+		$this->changeIdExtractor = $changeIdExtractor;
+	}
+
 	public function handle(Request $request, Response $response, array $args) {
-		$response->getBody()->write("Maybe...");
+		$gerritUrl = $args['gerriturl'];
+		$changeId = $this->changeIdExtractor->getChangeId( $gerritUrl );
+
+		$response->getBody()->write("Maybe... " . $changeId);
 	}
 
 }
--- web/index.php
+++ web/index.php
@@ -12,7 +12,9 @@ $container['slim_helloworld'] = function ($c) {
 	return new \Addtool\Slim\HelloWorld();
 };
 $container['slim_isitdeployed'] = function ($c) {
-	return new \Addtool\Slim\IsItDeployed();
+	return new \Addtool\Slim\IsItDeployed(
+		$c['wikimedia_gerrit_changeidextractor']
+	);
 };
 $container['wikimedia_gerrit_changeidextractor'] = function ($c) {
 	return new \Addtool\Wikimedia\Gerrit\UrlChangeIdExtractor();
@@ -21,6 +23,6 @@ $container['wikimedia_gerrit_changeidextractor'] = function ($c) {
 $app = new Slim\App();
 
 $app->get('/helloworld/{name}', [ $container['slim_helloworld'], 'handle' ] );
-$app->get('/isitdeployed/{gerriturl}', [ $container['slim_isitdeployed'], 'handle' ] );
+$app->get('/isitdeployed/{gerriturl:.*}', [ $container['slim_isitdeployed'], 'handle' ] );
 
 $app->run();
