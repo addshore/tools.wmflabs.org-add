e0d1005376b44e1bd8ad8bf8432e932df3952625
addshore <addshorewiki@gmail.com>
2018-11-03 15:44:03 +0000

    Split openapi spec into own file...

--- home/public_html/entry/api.php
+++ home/public_html/entry/api.php
@@ -10,76 +10,11 @@ require_once __DIR__ .
 $services = new \Addtool\Services();
 $app = new Slim\App();
 
-$getOpenApiSpecRequestHandler = function () {
-	// 2468 indicates a dev env (with docker)
-	if( $_SERVER['SERVER_PORT'] == 2468 ) {
-		$baseUrl = "http://localhost:3333/add/api/";
-	} else {
-		$baseUrl = "https://tools.wmflabs.org/add/api/";
-	}
-	return new \Addtool\Slim\OpenApiSpec(
-		new \erasys\OpenApi\Spec\v3\Document(
-			new \erasys\OpenApi\Spec\v3\Info(
-				'Add Tool', '0.0.1', 'A collection of Apis..'
-			),
-			[
-				'/spec' => new \erasys\OpenApi\Spec\v3\PathItem(
-					[
-						'get' => new \erasys\OpenApi\Spec\v3\Operation(
-							[
-								'200' => new \erasys\OpenApi\Spec\v3\Response( 'Successful response.' ),
-								'default' => new \erasys\OpenApi\Spec\v3\Response(
-									'Default error response.'
-								),
-							]
-						)
-					]
-				),
-				'/gerrit.wikimedia/deployedSites/{gerritchange}' => new \erasys\OpenApi\Spec\v3\PathItem(
-					[
-						'get' => new \erasys\OpenApi\Spec\v3\Operation(
-							[
-								'200' => new \erasys\OpenApi\Spec\v3\Response( 'Successful response.' ),
-								'default' => new \erasys\OpenApi\Spec\v3\Response(
-									'Default error response.'
-								),
-							]
-						),
-						'parameters' => [
-							new \erasys\OpenApi\Spec\v3\Parameter(
-								'gerritchange',
-								'path',
-								'Gerrit change URL',
-								[
-									'schema' => new \erasys\OpenApi\Spec\v3\Schema(
-										[
-											'type' => 'string',
-										]
-									),
-									'example' => 'https://gerrit.wikimedia.org/r/#/c/mediawiki/extensions/WikibaseQualityConstraints/+/470058/',
-								]
-							)
-						],
-					]
-				),
-			],
-			'3.0.1',
-			[
-				'servers' => [
-					new \erasys\OpenApi\Spec\v3\Server(
-						$baseUrl
-					)
-				]
-			]
-		)
-	);
-};
-
 $app->get('/add/api/helloworld/{name}', [ $services['slim_helloworld'], 'handle' ] );
 $app->get( '/add/api/changesfrombug/{bug}', [ $services['slim_changesfrombug'], 'handle' ] );
 $app->get(
 	'/add/api/spec',
-	[ $getOpenApiSpecRequestHandler(), 'handle' ]
+	[ new \Addtool\Slim\OpenApiSpec( require_once __DIR__ . DIRECTORY_SEPARATOR . 'spec.php'), 'handle' ]
 );
 $app->get(
 	'/add/api/gerrit.wikimedia/deployedSites/{gerriturl:.*}',
---
+++ home/public_html/entry/spec.php
@@ -0,0 +1,57 @@
+<?php
+
+// 2468 indicates a dev env (with docker)
+if ( $_SERVER['SERVER_PORT'] == 2468 ) {
+	$baseUrl = "http://localhost:3333/add/api/";
+} else {
+	$baseUrl = "https://tools.wmflabs.org/add/api/";
+}
+
+return new \erasys\OpenApi\Spec\v3\Document(
+	new \erasys\OpenApi\Spec\v3\Info(
+		'Add Tool', '0.0.1', 'A collection of Apis..'
+	), [
+		'/spec' => new \erasys\OpenApi\Spec\v3\PathItem(
+			[
+				'get' => new \erasys\OpenApi\Spec\v3\Operation(
+					[
+						'200' => new \erasys\OpenApi\Spec\v3\Response( 'Successful response.' ),
+						'default' => new \erasys\OpenApi\Spec\v3\Response(
+							'Default error response.'
+						),
+					]
+				),
+			]
+		),
+		'/gerrit.wikimedia/deployedSites/{gerritchange}' => new \erasys\OpenApi\Spec\v3\PathItem(
+			[
+				'get' => new \erasys\OpenApi\Spec\v3\Operation(
+					[
+						'200' => new \erasys\OpenApi\Spec\v3\Response( 'Successful response.' ),
+						'default' => new \erasys\OpenApi\Spec\v3\Response(
+							'Default error response.'
+						),
+					]
+				),
+				'parameters' => [
+					new \erasys\OpenApi\Spec\v3\Parameter(
+						'gerritchange', 'path', 'Gerrit change URL', [
+							'schema' => new \erasys\OpenApi\Spec\v3\Schema(
+								[
+									'type' => 'string',
+								]
+							),
+							'example' => 'https://gerrit.wikimedia.org/r/#/c/mediawiki/extensions/WikibaseQualityConstraints/+/470058/',
+						]
+					),
+				],
+			]
+		),
+	], '3.0.1', [
+		'servers' => [
+			new \erasys\OpenApi\Spec\v3\Server(
+				$baseUrl
+			),
+		],
+	]
+);
