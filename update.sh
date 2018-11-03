6ce55db8ae7a1851e4b263f2f9339c8bd1bb621e
addshore <addshorewiki@gmail.com>
2018-11-03 15:58:27 +0000

    update: use -T for dir copies

--- update.sh
+++ update.sh
@@ -15,8 +15,8 @@ else
 
     composer --working-dir=$HOME/src/ --no-dev --ignore-platform-reqs update
 
-    cp -R ~/src/home/public_html/entry ~/public_html/entry
-    cp -R ~/src/vendor/swagger-api/swagger-ui/dist ~/public_html/swagger-dist
+    cp -R -T ~/src/home/public_html/entry ~/public_html/entry
+    cp -R -T ~/src/vendor/swagger-api/swagger-ui/dist ~/public_html/swagger-dist
     cp -R ~/src/home/.lighttpd.conf ~/.lighttpd.conf
 
     webservice --backend kubernetes php7.2 restart
