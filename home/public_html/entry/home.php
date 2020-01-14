febc30fd3642f660155c1595e5a42df439bc3aec
addshore <addshorewiki@gmail.com>
2020-01-14 21:26:35 +0000

    home: note that swagger has a sansbox

--- home/public_html/entry/home.php
+++ home/public_html/entry/home.php
@@ -11,8 +11,8 @@ echo "<body>";
 echo "<h1>Welcome Home!</h1>";
 
 echo "<ul>";
+echo "<li><a href='{$baseUrl}/swagger/' >Swagger</a> (with API sandbox)</li>";
 echo "<li><a href='{$baseUrl}/api' >API Base</a> (you'll get a 404)</li>";
-echo "<li><a href='{$baseUrl}/swagger/' >Swagger</a></li>";
 echo "<li><a href='{$baseUrl}/api/spec' >Open API Spec</a></li>";
 echo "<li><a href='https://github.com/addshore/tools.wmflabs.org-add' >Github code</a></li>";
 echo "</ul>";
